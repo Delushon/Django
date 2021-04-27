90529f62a2c60c51ef0abb41015bc965c6285e9a
AlexanderP <a.panshin@elesta.ru>
2021-04-27 14:30:56 +0300

    extra context

--- hello/urls.py
+++ hello/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from django.views.generic import TemplateView
  
 urlpatterns = [
-    path('about/', TemplateView.as_view(template_name="firstapp/about.html")),
+    path('about/', TemplateView.as_view(template_name="firstapp/about.html", 
+        extra_context={"header": "О сайте"})),
     path('contact/', TemplateView.as_view(template_name="firstapp/contact.html")),
 ]
