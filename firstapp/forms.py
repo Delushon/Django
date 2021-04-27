548bf64ef98d8b9bd12844b339872afef8614909
AlexanderP <a.panshin@elesta.ru>
2021-04-27 17:02:10 +0300

    формы

--- firstapp/forms.py
+++ firstapp/forms.py
@@ -1,5 +1,6 @@
 from django import forms
- 
+
+
 class UserForm(forms.Form):
-    name = forms.CharField()
-    age = forms.IntegerField()
+    name = forms.CharField(label="Имя", initial="Иван", help_text="Введите свое имя")
+    age = forms.IntegerField(label="Возраст(лет)", initial=20, help_text="Введите свой возраст")
